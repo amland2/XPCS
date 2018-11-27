dbc92cf8a71cbaf8602f7be7b9a200b473694332
amland2 <amland2@illinois.edu>
2018-11-27 12:45:44 -0600

    Test Commit
    blah blah blah

---
+++ FourTimeCorrFunc.m
@@ -0,0 +1,12 @@
+%function [g4_data] = FourTimeCorrFunc(TwoTimeInfo)
+
+C=TwoTimeInfo.C{1,1};
+framespacing=TwoTimeInfo.framespacing;
+WaitingTime=(1:1:size(C,1)).*framespacing;
+
+gpu_C=gpuarray(C);
+    for index_WaitingTime
+        A
+    end
+%end
+
